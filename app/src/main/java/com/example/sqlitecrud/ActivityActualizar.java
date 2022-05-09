877ac33912cb370777d02ec5abcda7212ebb4951
axelcuaxilo <axelcuaxiloschool@gmail.com>
2022-05-08 19:26:03 -0500

    Added activity_eliminar.xml

--- app/src/main/java/com/example/sqlitecrud/ActivityActualizar.java
+++ app/src/main/java/com/example/sqlitecrud/ActivityActualizar.java
@@ -2,7 +2,11 @@ package com.example.sqlitecrud;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class ActivityActualizar extends AppCompatActivity {
 
@@ -10,5 +14,34 @@ public class ActivityActualizar extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_actualizar);
+        Button eliminar = (Button) findViewById(R.id.fab);
+        Button actualizar = (Button) findViewById(R.id.btnActualizar);
+
+        // get a database
+        DatabaseHelper databaseHelper = new DatabaseHelper(this);
+
+        //Get writable database
+        SQLiteDatabase database = databaseHelper.getWritableDatabase();
+
+        //check if database is created
+        if (database != null) {
+            //Create a toast message to show that database is created
+            //Toast.makeText(this, "Database is created", Toast.LENGTH_LONG).show();
+            //open a new activity
+            eliminar.setOnClickListener(v -> {
+                //Create a new intent to open the new activity
+                Intent intent = new Intent(ActivityActualizar.this, ActivityEliminar.class);
+                //Start the new activity
+                startActivity(intent);
+                //Close the current activity
+                finish();
+            });
+        }
+
+        //get readable database
+        database = databaseHelper.getReadableDatabase();
+        //retrieve all rows from table tarea
+        Cursor cursor = database.rawQuery("SELECT * FROM tarea where completada=0", null);
+
     }
 }
--- app/src/main/java/com/example/sqlitecrud/ActivityEliminar.java
+++ app/src/main/java/com/example/sqlitecrud/ActivityEliminar.java
@@ -2,7 +2,10 @@ package com.example.sqlitecrud;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class ActivityEliminar extends AppCompatActivity {
 
@@ -10,5 +13,38 @@ public class ActivityEliminar extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_eliminar);
+        Button aceptar = (Button) findViewById(R.id.btnAceptar);
+        Button cancelar = (Button) findViewById(R.id.btnRechazar);
+
+        // get a database
+        DatabaseHelper databaseHelper = new DatabaseHelper(this);
+
+        //Get writable database
+        SQLiteDatabase database = databaseHelper.getWritableDatabase();
+
+        //check if database is created
+        if (database != null) {
+            //Create a toast message to show that database is created
+            //Toast.makeText(this, "Database is created", Toast.LENGTH_LONG).show();
+            //open a new activity
+            aceptar.setOnClickListener(v -> {
+                //Create a new intent to open the new activity
+                Intent intent = new Intent(ActivityEliminar.this, ActivityVerTodo.class);
+                //Start the new activity
+                startActivity(intent);
+                //Close the current activity
+                finish();
+            });
+
+            //open a new activity
+            cancelar.setOnClickListener(v -> {
+                //Create a new intent to open the new activity
+                Intent intent = new Intent(ActivityEliminar.this, ActivityActualizar.class);
+                //Start the new activity
+                startActivity(intent);
+                //Close the current activity
+                finish();
+            });
+        }
     }
 }
